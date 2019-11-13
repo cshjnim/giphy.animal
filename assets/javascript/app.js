0dba8d3a3c48ef35dfd2718a6b66c5988828d01f
Hyejin Kim <cshjkin@gmail.com>
2019-11-12 23:27:59 -0800

    adding basic folders and files and add basic basic layouts

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+    // queryURL for Giphy API
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
