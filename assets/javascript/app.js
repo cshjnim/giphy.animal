3e8e947766d6f1248464de8d51da90e3093c65c9
Hyejin Kim <cshjkin@gmail.com>
2019-11-14 18:17:33 -0800

    updating the html to see if this works and update with right api key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,18 @@
     }).then(function(response) {
       console.log(response);
     });
+
+
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(pizza) {
+      console.log(pizza.data);
+      var array = pizza.data;
+      for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        $("body").append(`<img src='${element.images.downsized.url}'>`);
+      }
+    });
