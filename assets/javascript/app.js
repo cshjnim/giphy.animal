5ed47a47897672217c9d0b5c2ce0fd902c101c3b
Hyejin Kim <cshjkin@gmail.com>
2019-11-15 22:26:07 -0800

    revise the ajax to make sure function works when certain animal button pressed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,24 @@
-    // queryURL for Giphy API
 
+// Adding click event
+  $("button").on("click", function() {
+    var animal = $(this).attr("data-animal");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=DSe9hrdaQyo7dWmrOeCmLF3Su4mqHybG&limit=25&rating=G";
+    // searchable url (set limit to 10images)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=DSe9hrdaQyo7dWmrOeCmLF3Su4mqHybG&limit=10";
 
+  //ajax!
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-      console.log(reponse.data);
-      var array = response.data;
-      for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        $("body").append(`<img src='${element.images.downsized.url}'>`);
-      }
-    });
+    })
+      .then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+        // storing the data by set up a new result variable
+        var results = response.data;
+
+        animalImage.attr("src", results);
+        }
+      ) 
+  );
