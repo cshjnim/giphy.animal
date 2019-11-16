65cb7cd913cd7428374f4b989e073599fdaa11bd
Hyejin Kim <cshjkin@gmail.com>
2019-11-15 22:41:58 -0800

    found wrong function and change to for looping

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,18 @@
         // storing the data by set up a new result variable
         var results = response.data;
 
-        animalImage.attr("src", results);
-        }
-      ) 
-  );
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
+        // Creating and storing a div
+        var animalDiv = $("<div>");
+        // Creating and storing images
+        var animalImage = $("<img>");
+        // Setting the src attribute of the image
+        animalImage.attr("src", results[i].images.fixed_height.url);
+      
+        animalDiv.append(animalImage);
+        // Prependng the animalDiv to the HTML page
+        $("#images").prepend(animalDiv);
+      }
+    })
+  });
