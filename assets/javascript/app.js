547562bf66760f25c50db3b9e70e460ca4fbb6c7
Hyejin Kim <cshjkin@gmail.com>
2019-11-15 21:37:56 -0800

    updating the style css and app jquery to see if ajax works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,16 @@
     // queryURL for Giphy API
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+
+
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=DSe9hrdaQyo7dWmrOeCmLF3Su4mqHybG&limit=25&rating=G";
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
-    });
-
-
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(pizza) {
-      console.log(pizza.data);
-      var array = pizza.data;
-      for (let index = 0; index < array.length; index++) {
-        const element = array[index];
+      console.log(reponse.data);
+      var array = response.data;
+      for (let i = 0; i < array.length; i++) {
+        const element = array[i];
         $("body").append(`<img src='${element.images.downsized.url}'>`);
       }
     });
